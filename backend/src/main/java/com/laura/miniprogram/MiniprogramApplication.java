bcc9714dd6dca1cefd2366462bdf30cb627e3ddf
Rui Zhao <christianoray@gmail.com>
2021-09-13 10:55:00 +0800

    init

---
+++ backend/src/main/java/com/laura/miniprogram/MiniprogramApplication.java
@@ -0,0 +1,13 @@
+package com.laura.miniprogram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniprogramApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiniprogramApplication.class, args);
+    }
+
+}
